905a863f4179155527ff8b15b6f51a85b037243e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-09 19:32:44 +0900

    sequelize 설정 - sequelize package 설치 - app.js에서 express 연동 - index.js에서 모델 생성하여 db객체에 대입

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const express = require('express');
-const data = require('/data');
+const express = require('express'); // nodejs express 서버 모듈
+const path = require('path'); // 파일 디렉토리 모듈 
+const morgan = require('morgan'); // 요청 로그 기록
+
+const { sequelize } = require('./models');
+
+const app = express();
